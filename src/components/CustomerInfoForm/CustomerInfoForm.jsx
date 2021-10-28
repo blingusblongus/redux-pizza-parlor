75815c25bce93ddf99c52c74075785cdd50eb329
Nick Allen <rutherford.t.b@gmail.com>
2021-10-28 11:29:47 -0500

    CustomerInfoForm complete, except for redux hookup

--- src/components/CustomerInfoForm/CustomerInfoForm.jsx
+++ src/components/CustomerInfoForm/CustomerInfoForm.jsx
@@ -6,36 +6,60 @@ function CustomerInfoForm(props) {
     const [city, setCity] = useState('');
     const [address, setAddress] = useState('');
     const [zip, setZip] = useState('');
-    
+    // type is Pickup by default
+    const [type, setType] = useState('Pickup');
+
     //This will need to obtain the cart info,
     //combine it with the form info,
     //and DISPATCH it to the order reducer
     const handleSubmit = () => {
 
-
     }
 
+    const handleRadio = (e) => {
+            setType(e.target.value);
+    }
+
+    console.log(type);
+
     return (
-        <div>
+        <div id="info-page">
             <h2>Step 2: Customer Information</h2>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <input className="input" type="text" placeholder="Name" 
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}></input>
-                    <input className="input" type="text" 
-                    placeholder="Street Address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}></input>
-                    <input className="input" type="text" placeholder="City"
-                    onChange={(e) => setCity(e.target.value)}
-                    value={city}></input>
-                    <input className="input" type="text" placeholder="Zip"
-                    onChange={(e) => setZip(e.target.value)}
-                    value={zip}></input>
-                </div>
-                <div>
-                    <input type="radio"></input>
+                <div className="flex-container">
+                    <div id="text-container">
+                        <input className="input" type="text" placeholder="Name"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}></input>
+                        <input className="input" type="text"
+                            placeholder="Street Address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}></input>
+                        <input className="input" type="text" placeholder="City"
+                            onChange={(e) => setCity(e.target.value)}
+                            value={city}></input>
+                        <input className="input" type="text" placeholder="Zip"
+                            onChange={(e) => setZip(e.target.value)}
+                            value={zip}></input>
+                    </div>
+
+                    {/* Radio buttons update state onChange, using value attr */}
+                    <div id="radio-container">
+                        <div>
+                            <input type="radio" id="pickup" name="type" 
+                            onChange={handleRadio} defaultChecked={true}
+                            value="Pickup"></input>
+                            <label htmlFor="pickup">Pickup</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="delivery" name="type"
+                            onChange={handleRadio}
+                            value="Delivery"></input>
+                            <label htmlFor="delivery">Delivery</label>
+                        </div>
+                        
+                    </div>
+                    <button type="submit">NEXT</button>
                 </div>
             </form>
         </div>
