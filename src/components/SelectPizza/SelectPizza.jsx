5dc336353b9c0213c5830515491690b8148ee75b
Jordan Ponto <jponto93@gmail.com>
2021-10-28 14:28:26 -0500

    started routing buttons, one is added on SelectPizza page

--- src/components/SelectPizza/SelectPizza.jsx
+++ src/components/SelectPizza/SelectPizza.jsx
@@ -1,12 +1,20 @@
 import PizzaListItem from "../PizzaListItem/PizzaListItem"
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router"
 
 function SelectPizza () {
 
     const pizzaList = useSelector(store => store.pizzaList)
+    const history = useHistory();
     // console.log('SelectPizza', pizzaList);
+
+    const toInfo = () => {
+        console.log('inside toInfo');
+        // history.push('/info')
+    } // end toInfo
     
     return (
+        <>
         <div>
             {pizzaList.map((pizza) => {
                 return (
@@ -16,6 +24,10 @@ function SelectPizza () {
                 )
             })}
         </div>
+        <div>
+            <button onClick={toInfo}>NEXT</button>
+        </div>
+        </>
     )
 } // end SelectPizza
 
