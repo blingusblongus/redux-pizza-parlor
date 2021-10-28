40dabe5a05e4b3ac5420b735d3e4ecd235fc0f27
Jack Barrett <jbarrett1347@gmail.com>
2021-10-28 11:03:08 -0500

    Added header and nav fucntionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Header from '../Header/Header.jsx'
+// import SelectPizza from "../SelectPizza/SelectPizza.jsx";
+// import CustomerInfoForm from '../CustomerInfoForm.jsx';
+// import Checkout from '../Checkout/Checkout.jsx';
+// import Admin from '../Admin/Admin.jsx';
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        {/* <Route path="/">
+          <SelectPizza />
+        </Route>
+
+        <Route path="/info">
+          <CustomerInfoForm />
+        </Route>
+
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+
+        <Route path="/admin">
+            <Admin />
+        </Route> */}
+
+        <img src="images/pizza_photo.png" />
+        <p>Pizza is great.</p>
+      </div>
+    </Router>
   );
 }
 
